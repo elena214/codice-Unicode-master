b56f85bac22877c03ad9476abe32396c71a652e2
elena214 <elebe2104@gmail.com>
2020-11-26 12:16:51 +0100

    ramosviluppo

--- codice Unicode/Program.cs
+++ codice Unicode/Program.cs
@@ -6,16 +6,18 @@ namespace codice_Unicode
     {
         static void Main(string[] args)
         {
-            String unicodeString = 
-                "This Unicode string has 2 characters outside the " + 
-                "ASCII range: \n" + 
-                "Pi (\u03A0), and Sigma (\u03A3).";
-            Console.WriteLine("Original string:");
+            //String unicodeString = 
+            //    "This Unicode string has 2 characters outside the " + 
+            //    "ASCII range: \n" + 
+            //    "Pi (\u03A0), and Sigma (\u03A3).";
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            string unicodeString = "";
+            Console.WriteLine("Scrivi un messaggio");
+            unicodeString = Console.ReadLine();
+            Console.WriteLine("Original String");
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
             Console.WriteLine();
-            Console.WriteLine("Inserisci una frase");
-            Console.ReadLine(); // per inserire a console la frase
         }
     }
 }
