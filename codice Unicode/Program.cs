8ca816077ad156d9096e7064641b3905f9c31329
elena214 <elebe2104@gmail.com>
2020-11-26 11:54:08 +0100

    Aggiungere i file di progetto.

---
+++ codice Unicode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace codice_Unicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String unicodeString = 
+                "This Unicode string has 2 characters outside the " + 
+                "ASCII range: \n" + 
+                "Pi (\u03A0), and Sigma (\u03A3).";
+            Console.WriteLine("Original string:");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodeString);
+            Console.WriteLine();
+            Console.WriteLine("Inserisci una frase");
+            Console.ReadLine(); // per inserire a console la frase
+        }
+    }
+}
